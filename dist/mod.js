83847f09e28a40125c8781a75a4a2891722b132d
ddu6 <ddu6@protonmail.com>
2021-05-29 02:50:31 +0800

    update

--- dist/mod.js
+++ dist/mod.js
@@ -501,7 +501,7 @@ async function unlock() {
                 }] } });
     const page = await context.newPage();
     try {
-        await page.goto('https://pkuhelper.pku.edu.cn/hole', { timeout: init_1.config.unlockingSleep });
+        await page.goto('https://pkuhelper.pku.edu.cn/hole', { timeout: init_1.config.unlockingSleep * 1000 });
     }
     catch (err) {
         log(err);
